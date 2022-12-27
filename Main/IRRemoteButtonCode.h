3bdad13528b8a865d2a9800ad4e4f5166ecfd42c
Anupriya Srivastava <rahulnigamfzd@yahoo.com>
2022-12-27 18:16:50 +0530

    TV remote keys added.

--- Main/IRRemoteButtonCode.h
+++ Main/IRRemoteButtonCode.h
@@ -2,9 +2,47 @@
 #define IRRemoteButtonCode_h
 
 #define TV_REMOTE_POWERKEY 0xF708FB04
-
-
-
-
+#define TV_REMOTE_RightKey 0xF906FB04
+#define TV_REMOTE_LeftKey 0xF807FB04
+#define TV_REMOTE_ForwardKey 0xBF40FB04
+#define TV_REMOTE_ratio 0xA66EB7B0
+#define TV_REMOTE_mute 0xF609FB04
+#define TV_REMOTE_ok 0xBB44FB04
+#define TV_REMOTE_0 0xEF10FB04
+#define TV_REMOTE_1 10xEE11FB04
+#define TV_REMOTE_2 0xED12FB04
+#define TV_REMOTE_3 0xEC13FB04
+#define TV_REMOTE_4 0xEB14FB04
+#define TV_REMOTE_5 0xEA15FB04
+#define TV_REMOTE_6 0xE916FB04
+#define TV_REMOTE_7 0xE817FB04
+#define TV_REMOTE_8 0xE718FB04
+#define TV_REMOTE_9 0xE619FB04
+#define TV_REMOTE_q_menu 0xBA45FB04
+#define TV_REMOTE_manu 0xBC43FB04
+#define TV_REMOTE_red 0x8D72FB04
+#define TV_REMOTE_green 0x8E71FB04
+#define TV_REMOTE_yellow 0x9C63FB04
+#define TV_REMOTE_blue 0x9E61FB04
+#define TV_REMOTE_stop 0x4EB1FB04
+#define TV_REMOTE_play 0x45BAFB04
+#define TV_REMOTE_pose 0x4FB0FB04
+#define TV_REMOTE_back 0x708FFB04
+#define TV_REMOTE_forward 0x708FFB04
+#define TV_REMOTE_energySaving 0x6A95FB04
+#define TV_REMOTE_AVMode 0xCF30FB04
+#define TV_REMOTE_input 0xF40BFB04
+#define TV_REMOTE_tv 0xF00FFB04
+#define TV_REMOTE_mark_fav 0xE11EFB04
+#define TV_REMOTE_list 0xAC53FB04
+#define TV_REMOTE_Plus_v 0x102FB04
+#define TV_REMOTE_Mnus_v 0x102FB04
+#define TV_REMOTE_PLUS_p 0xFF00FB04
+#define TV_REMOTE_Minus_p 0xFE01FB04
+#define TV_REMOTE_Xstudio 0x837CFB04
+#define TV_REMOTE_smplink 0x817EFB04
+#define TV_REMOTE_return 0xD728FB04
+#define TV_REMOTE_text 0xDF20FB04
+#define TV_REMOTE_t_opt 0xDE21FB04
 
 #endif
