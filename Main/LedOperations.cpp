292b589a4d87091884df14ae1589057cef0701b8
Anupriya Srivastava <rahulnigamfzd@yahoo.com>
2022-12-27 19:20:23 +0530

    led light code added.

--- Main/LedOperations.cpp
+++ Main/LedOperations.cpp
@@ -1,14 +1,75 @@
 #include <Arduino.h>
 #include "LedOperations.h"
 
+/*#define RELAY_1 53
+#define RELAY_2 51
+#define RELAY_3 49
+#define RELAY_4 47
+#define RELAY_5 45
+#define RELAY_6 43
+#define RELAY_7 41
+#define RELAY_8 39
+*/
 LedOperations::LedOperations()
 {
     LOG_LedOperations("LedOperations::LedOperations()");
+    pinMode(RELAY_1, OUTPUT);
+    pinMode(RELAY_2, OUTPUT);
+    pinMode(RELAY_3, OUTPUT);
+    pinMode(RELAY_4, OUTPUT);
+    pinMode(RELAY_5, OUTPUT);
+    pinMode(RELAY_6, OUTPUT);
+    pinMode(RELAY_7, OUTPUT);
+    pinMode(RELAY_8, OUTPUT);
+    digitalWrite(RELAY_1, HIGH);    
+    digitalWrite(RELAY_2, HIGH);    
+    digitalWrite(RELAY_3, HIGH);    
+    digitalWrite(RELAY_4, HIGH);    
+    digitalWrite(RELAY_5, HIGH);    
+    digitalWrite(RELAY_6, HIGH);    
+    digitalWrite(RELAY_7, HIGH);    
+    digitalWrite(RELAY_8, HIGH);
 }
 
 void LedOperations::setup()
 {
+    int dely_dw = 200;
+    int dwStatus = LOW;
     LOG_LedOperations("LedOperations::setup()");
+    digitalWrite(RELAY_1, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_2, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_3, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_4, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_5, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_6, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_7, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_8, dwStatus);
+    delay(dely_dw);
+
+    dwStatus = HIGH;
+    digitalWrite(RELAY_1, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_2, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_3, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_4, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_5, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_6, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_7, dwStatus);
+    delay(dely_dw);
+    digitalWrite(RELAY_8, dwStatus);
+    delay(dely_dw);
 }
 
 void LedOperations::loop()
